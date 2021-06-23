6bd25b9327181c626e996440e8594ebacb44b65f
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-06-22 21:07:19 -0300

    Comentário no código

--- exercicios_das_aulas/aula_11/cadastro_de_funcionario.py
+++ exercicios_das_aulas/aula_11/cadastro_de_funcionario.py
@@ -1,7 +1,7 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário. Se por acaso a CTPS for diferente de 0, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente , além da idade , com quantos anos a pessoa vai se aposentar. Considere que o trabalhador deve contribuir por 35 anos para se aposentar.
 
 #Cadastro de Funcionários alpha por João Rodrigues
-#Importando biblioteca OS para limpar a tela
+#Importando biblioteca OS e SYS para limpar a tela
 import os
 import sys
 if sys.platform.startswith('win32') or sys.platform.startswith('cigwin'):
