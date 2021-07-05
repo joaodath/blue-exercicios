facec10c9588bedd4281b6910fc88139463fae8c
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-07-05 15:19:41 -0300

    improving aula_16

--- aula_16/contabancaria.py
+++ aula_16/contabancaria.py
@@ -1,21 +1,60 @@
 class Conta:
-    def __init__(self, titular, saldo):
-        self.Titular = titular
-        self.Saldo = saldo
-    def sacar(self, valor):
-        if self.Saldo > 0 and valor < self.Saldo:
-            self.Saldo -= valor
+    def __init__(self, nome = '', saldo = 0):
+        self.__titular_da_conta = nome
+        self.__saldo = saldo
+    
+    def __str__(self):
+        if self.__saldo > 0:
             return f'''
-            O saldo em conta é R${self.Saldo + valor}.
-            O valor sacado foi R${valor}.
-            O novo saldo em conta é R${self.Saldo}.'''
+            Titular: {self.__titular_da_conta}
+            Saldo: {self.__saldo}'''
         else:
             return f'''
-            Valor em conta R${self.Saldo} insuficiente.
+            Titular: {self.__titular_da_conta}'''
+    
+    def __pode_sacar(self, valor_saque):
+        return self.__saldo > valor_saque
+
+    def sacar(self, valor):
+        if self.__pode_sacar:
+            self.__saldo -= valor
+            return f'''
+            O saldo em conta é R${self.__saldo + valor}.
+            O valor sacado foi R${valor}.
+            O novo saldo em conta é R${self.__saldo}.'''
+        else:
+            return f'''
+            Valor em conta R${self.__saldo} insuficiente.
             Você não tem saldo suficiente para essa operação.'''
+    
     def depositar(self, valor):
-        self.Saldo += valor
+        self.__saldo += valor
         return f'''
-        O saldo em conta é R${self.Saldo - valor}.
+        O saldo em conta é R${self.__saldo - valor}.
         O valor depositado foi R${valor}.
-        O novo saldo em conta é R${self.Saldo}.'''
+        O novo saldo em conta é R${self.__saldo}.'''
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular_da_conta
+
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
+    
+    @titular.setter
+    def set_titular(self, nome):
+        self.__titular_da_conta = nome
+
+cliente01 = Conta()
+cliente01.set_titular('João')
+cliente01.saldo()
+saque = float(input(f'Informe o valor a ser sacado em Reais da conta de {cliente01.titular}. R$'))
+print(cliente01.sacar(saque))
+deposito = float(input(f'Informe o valor a ser depositado em Reais da conta de {cliente01.titular}. R$'))
+print(cliente01.depositar(saque))
+print('Fim do Programa!')
--- aula_16/exercicio_02.py
+++ aula_16/exercicio_02.py
@@ -1,9 +1,16 @@
 #Crie uma classe chamada Conta para simular as operações de uma conta corrente. Sua classe deverá ter os atributos Titular e Saldo, e os métodos Sacar e Depositar. Crie um objeto da classe Conta e teste os atributos e métodos implementados.​ Adicione uma regra no método Sacar, onde o usuário só poderá sacar se o Saldo for maior que zero, caso contrário mostre a mensagem na tela: "Você não tem saldo suficiente para essa operação."
-from .contabancaria import Conta
+conta_import = str(input('Importar conta? S/N ').strip().upper())
+if conta_import[0] == 'S':
+    from contabancaria import *
+    print('Importei!')
+else:
+    print('Não importei!')
 
-cliente01 = Conta(str(input('Informe o nome do novo cliente: ')), float(input('Informe o saldo inicial do novo cliente em Reais: R$')))
-saque = float(input(f'Informe o valor a ser sacado em Reais da conta de {cliente01.Titular}. R$'))
+cliente01 = Conta()
+#cliente01.titular(input('Informe o nome completo do titular da conta: ').strip().capitalize())
+cliente01.saldo()
+saque = float(input(f'Informe o valor a ser sacado em Reais da conta de {cliente01.titular}. R$'))
 print(cliente01.sacar(saque))
-deposito = float(input(f'Informe o valor a ser depositado em Reais da conta de {cliente01.Titular}. R$'))
+deposito = float(input(f'Informe o valor a ser depositado em Reais da conta de {cliente01.titular}. R$'))
 print(cliente01.depositar(saque))
 print('Fim do Programa!')
